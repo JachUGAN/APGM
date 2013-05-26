04172c0662ba82993efd0411d9297a21f5cf706a
Jach <jachugan@gmail.com>
2013-05-26 17:22:07 +0100

    First Commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,7 @@
+class PostsController < ApplicationController
+  def home
+  end
+
+  def new
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+APGM::Application.config.secret_token = 'c85a198c451baa8cfff5323f829c537f8340b4321fbf66ba5d2fa89d9ca7f4181329897a56eb5cca26e493b4d35e6ec4bb16bfde43fb8a906693061b4fef8051'
